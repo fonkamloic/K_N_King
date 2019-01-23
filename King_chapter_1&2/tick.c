08fbb19bf4cc494cc7b0edae0bc11ab7bafe0d7b
fonkamloic <fonkamloic@gmail.com>
2019-01-24 00:15:04 +0100

    tick

---
+++ King_chapter_1&2/tick.c
@@ -0,0 +1,13 @@
+//Name:  tick
+//Purpose: prints out a tick
+//date created: 2 dec 2016
+
+#include <stdio.h>
+
+int main(void) //start of program
+{
+	//print stars
+  putc("       *\n      *\n     *\n*   *\n * *\n  *\n");
+   
+  return 0; // ends program
+}

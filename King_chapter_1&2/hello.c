28be691f2b5b63f0ea3da2265762d178883b8478
fonkamloic <fonkamloic@gmail.com>
2019-01-24 00:14:09 +0100

    hello

---
+++ King_chapter_1&2/hello.c
@@ -0,0 +1,11 @@
+/******************************************
+ *Name: hello                             *
+ * Purpose: prints hello world            *
+ *Date created: 2 Dec 2016                *
+ *****************************************/
+#include <stdio.h>
+
+int main(void)  // start of main function
+{
+    printf("hello, world\n");  // prints hello onto the screen
+}

69e8694699fb47cd37f7a02147f63bb38d80beac
fonkamloic <fonkamloic@gmail.com>
2019-01-24 00:14:46 +0100

    notInitialized

---
+++ King_chapter_1&2/notInitialized.c
@@ -0,0 +1,26 @@
+//Name: notInitialized
+//Purpose: prints out variable that have not been initialized
+//Date created: 2 dec 2016
+
+#include <stdio.h>
+
+int main(void)   // start of program
+{
+  int a, b, c, d, e, f, g, h, j, k, l, m, n, o, p, q;  // declaration of several variables
+  printf("The value of variable a is %d\n", a);  //printings the variables
+  printf("The value of variable b is %d\n", b);
+  printf("The value of variable c is %d\n", c);
+  printf("The value of variable d is %d\n", d);
+  printf("The value of variable e is %d\n", e);
+  printf("The value of variable f is %d\n", f);
+  printf("The value of variable g is %d\n", g);
+  printf("The value of variable j is %d\n", h);
+  printf("The value of variable k is %d\n", j);
+  printf("The value of variable l is %d\n", k);
+  printf("The value of variable m is %d\n", l);
+  printf("The value of variable n is %d\n", m);
+  printf("The value of variable o is %d\n", n);
+  printf("The value of variable p is %d\n", p);
+  printf("The value of variable q is %d\n", q);
+  return 0;  // ends program
+}
